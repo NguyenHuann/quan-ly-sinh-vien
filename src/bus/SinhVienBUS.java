3cbc865b3031e46af10517c2a2ad3f4722d3ed9c
NguyenHuan <huann6152@gmail.com>
2025-12-28 21:37:40 +0700

    feat: add getAllSinhVien, getSinhVienByLop, searchSinhVien, addSinhVien,

--- src/bus/SinhVienBUS.java
+++ src/bus/SinhVienBUS.java
@@ -3,19 +3,35 @@ package bus;
 import dao.SinhVienDAO;
 import dto.SinhVien;
 
+import java.util.List;
+
 public class SinhVienBUS {
-    SinhVienDAO dao = new SinhVienDAO();
-    public String themSinhVien(SinhVien sv) {
-        if (dao.existById(sv.getMaSV())) {
+    SinhVienDAO sinhVienDAO = new SinhVienDAO();
+
+//    lay toan bo danh sach sinh vien
+    public List<SinhVien> getAllSinhVien() {
+        return sinhVienDAO.getAll();
+    }
+//    lay sinh vien theo lop
+    public List<SinhVien> getSinhVienByLop(String maLop) {
+        return sinhVienDAO.getByLop(maLop);
+    }
+//    tim kiem sinh vien
+    public List<SinhVien> searchSinhVien(String keyword) {
+        return sinhVienDAO.search(keyword);
+    }
+    public String addSinhVien(SinhVien sv) {
+        if (sv.getMaSV().isEmpty() || sv.getHo().isEmpty() || sv.getTen().isEmpty()) {
+            return "Vui lòng nhập đầy đủ Mã, Họ, Tên";
+        }
+        if (sinhVienDAO.existById(sv.getMaSV())) {
             return "Mã sinh viên đã tồn tại";
         }
-        if (sv.getTen().isEmpty()) {
-            return "Tên không được để trống!";
-        }
-        if (dao.insert(sv)) {
+        if (sinhVienDAO.insert(sv)) {
             return "Thêm thành công!";
         }else {
             return "Lỗi khi thêm vào CSDL";
         }
     }
+
 }
