6da9fe6e2e9c83554362f28170ea1943cb27e096
NguyenHuan <huann6152@gmail.com>
2026-01-03 10:02:21 +0700

    refactor: change DB name

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/qlsv";
+        String url = "jdbc:mysql://localhost:3306/quanlysinhvien";
         String user = "root";
         String password = "";
         return DriverManager.getConnection(url, user, password);
